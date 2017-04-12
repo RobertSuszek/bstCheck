0b704425b4d29474013a1e4325845d91747c76ce
Robert Suszek <robert.suszek96@gmail.com>
2017-04-12 13:37:05 +0200

    added another files for prev commit

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//structure for holding a single tree node
 struct node {
     int value;
     struct node *parent;
@@ -8,19 +9,42 @@ struct node {
     struct node *right;
 };
 
-struct node *createNode();
-void insertNode(struct node **root, struct node *node);
+//returns pointer to allocated node and fills it with value
+struct node *createNode(int value);
 
-struct node *createNode() {
-    struct node *newNode = (struct node*) malloc(sizeof(node));
+//if none of childs is null, does nothing and returns 1, otherwise inserts node into tree and returns 0
+int insertNode(struct node **root, struct node *node);
+
+//adds node with specified value into tree (based on insertNode)
+int addNode(struct node **root, int value);
+
+//returns pointer to allocated node and fills it with value
+struct node *createNode(int value) {
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
     newNode->parent = NULL;
     newNode->left = NULL;
     newNode->right = NULL;
-    newNode->value = 0;
+    newNode->value = value;
+    return newNode;
 }
 
-void insertNode(struct node **root, struct node *node) {
-    struct node *newNode = createNode();
+//if none of childs is null, does nothing and returns 1, otherwise inserts node into tree and returns 0
+int insertNode(struct node **root, struct node *node) {
+    if ((*root)->left == NULL) {
+        node->parent = (*root);
+        (*root)->left = node;
+        return 0;
+    } else if ((*root)->right == NULL) {
+        node->parent = (*root);
+        (*root)->right = node;
+        return 0;
+    } else
+        return 1;
+}
+
+int addNode(struct node **root, int value) {
+    struct node *newNode = createNode(value);
+    return 1;
 }
 
 int main()
