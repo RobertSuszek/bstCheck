e061c08e74baf693059cc36f410f65b2f1066848
Robert Suszek <robert.suszek96@gmail.com>
2017-04-12 20:16:39 +0200

    didnt figured out how to read tree from file

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //structure for holding a single tree node
 struct node {
@@ -12,13 +13,12 @@ struct node {
 //returns pointer to allocated node and fills it with value
 struct node *createNode(int value);
 
-//if none of childs is null, does nothing and returns 1, otherwise inserts node into tree and returns 0
-int insertNode(struct node **root, struct node *node);
-
 //adds node with specified value into tree (based on insertNode)
 int addNode(struct node **root, int value);
 
-//returns pointer to allocated node and fills it with value
+//returns next line of file
+char *getNextLine(FILE *file);
+
 struct node *createNode(int value) {
     struct node *newNode = (struct node*) malloc(sizeof(struct node));
     newNode->parent = NULL;
@@ -27,30 +27,37 @@ struct node *createNode(int value) {
     newNode->value = value;
     return newNode;
 }
+int addNode(struct node **root, int value) {
+    struct node *newNode = createNode(value);
+    (*root) = newNode;
+}
 
-//if none of childs is null, does nothing and returns 1, otherwise inserts node into tree and returns 0
-int insertNode(struct node **root, struct node *node) {
-    if ((*root)->left == NULL) {
-        node->parent = (*root);
-        (*root)->left = node;
-        return 0;
-    } else if ((*root)->right == NULL) {
-        node->parent = (*root);
-        (*root)->right = node;
-        return 0;
+void analyzeLine(struct node **root, FILE **file) {
+    char line[256];
+    int value;
+    if (ferror((*file)) == 0) {
+        fgets(line, 256, (*file));
+        if (strchr(line, '/') == NULL) {
+            strtok(line, "<node=");
+            value = atoi(line);
+            printf("Dodaje node %i\n", value);
+            addNode(root, value);
+            analyzeLine(&((*root)->left), file);
+        }
+    }
+}
+
+int readTreeFromFile(struct node **root, char *path) {
+    FILE *file = fopen(path, "r");
+    if (file != NULL) {
+
     } else
         return 1;
 }
 
-int addNode(struct node **root, int value) {
-    struct node *newNode = createNode(value);
-    return 1;
-}
-
 int main()
 {
-    struct node *root;
-
+    struct node *root = NULL;
 
     return 0;
 }
